76e963be4aed1926e7fd8c5bdbcee80e12990c83
Joshdavid86 <35547456+Joshdavid86@users.noreply.github.com>
2022-01-03 17:42:01 -0600

    Add files via upload

---
+++ CodeTest.java
@@ -0,0 +1,25 @@
+package com.example.codetest;
+
+public class CodeTest {
+    public static void main(String[] args) {
+
+            int num1 = 1;
+            int num2 = 1;
+            int num3 = 0;
+
+            if (num1 + num2 == num3) {
+                System.out.println("+");
+            }
+            if (num1 - num2 == num3) {
+                System.out.println("-");
+            }
+            if (num1 * num2 == num3) {
+                System.out.println("*");
+            }
+            if (num1 / num2 == num3) {
+                System.out.println("/");
+            } else {
+                System.out.println("None");
+            }
+        }
+    }
